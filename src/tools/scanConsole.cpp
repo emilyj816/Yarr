864684e7f3e7dfbebfc11a4154ec315e9d8ce90f
emilyj816 <40067724+emilyj816@users.noreply.github.com>
2019-06-20 10:39:59 -0700

    Update scanConsole.cpp

--- src/tools/scanConsole.cpp
+++ src/tools/scanConsole.cpp
@@ -71,7 +71,6 @@ int main(int argc, char *argv[]) {
     std::cout << "\033[1;31m#####################################\033[0m" << std::endl;
     std::cout << "\033[1;31m# Welcome to the YARR Scan Console! #\033[0m" << std::endl;
     std::cout << "\033[1;31m#####################################\033[0m" << std::endl;
-    std::cout << "\033[1;31m#####################################\033[0m" << std::endl;
 
     std::cout << "\033[1;-> Parsing command line parameters ..." << std::endl;
     
