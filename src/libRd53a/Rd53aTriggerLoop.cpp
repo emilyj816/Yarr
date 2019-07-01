7fd5a51c128ead425d4e0b62265333830c4bf00e
emilyj816 <40067724+emilyj816@users.noreply.github.com>
2019-07-01 14:37:09 -0700

    Update Rd53aTriggerLoop.cpp

--- src/libRd53a/Rd53aTriggerLoop.cpp
+++ src/libRd53a/Rd53aTriggerLoop.cpp
@@ -34,10 +34,10 @@ Rd53aTriggerLoop::Rd53aTriggerLoop() : LoopActionBase() {
 }
 
 void Rd53aTriggerLoop::setTrigDelay(uint32_t delay) {
-    std::cout<<delay<<std::endl;
-    int offset = -8;
+    int digitalOffset = 0;
+    int analogOffset = 0;
+    int offset = 0;
     delay += offset;
-    std::cout<<delay<<std::endl;
     m_trigWord.fill(0x69696969);
     // Inject
     //m_trigWord[31] = 0x5a5a6969;
