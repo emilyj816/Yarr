e827cb480137cf14d078c93d18e1541b1472bbf9
emilyj816 <40067724+emilyj816@users.noreply.github.com>
2019-07-02 16:39:46 -0700

    Update Rd53aEmu.cpp

--- src/libEmu/Rd53aEmu.cpp
+++ src/libEmu/Rd53aEmu.cpp
@@ -612,6 +612,7 @@ void Rd53aEmu::triggerAsync0( const uint32_t tag) {
                 if(m_feCfg->InjEnDig.read() == 1){
                     
                     calculateSignalDig< PixelModel<Rd53aLinPixelModel> >( pixel, coreCol, coreRow, icol, irow, tag ); //digital hardcoding ToT = 8
+                    continue;
                     
                 } if( pixel.type() == typeid( PixelModel<Rd53aLinPixelModel>) ) {
                 
